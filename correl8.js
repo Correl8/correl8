6f25f5f45b68cebf5677bc57992cd6ee125f5ebe
Samuel Rinnetmäki <samuel@iki.fi>
2019-08-12 16:26:41 +0300

    got rid of types

--- correl8.js
+++ correl8.js
@@ -1,7 +1,7 @@
 const { Client } = require('@elastic/elasticsearch')
 
 // set range for numeric timestamp guessing
-const SECONDS_PAST = 60 * 60 * 24 * 365.25 * 10; // about 10 years in the past
+const SECONDS_PAST = 60 * 60 * 24 * 365.25 * 25; // about 25 years in the past
 const SECONDS_FUTURE = 60 * 60 * 24; // 24 hours in the future
 
 const correl8 = function(doctype, basename, clientOpts) {
@@ -15,10 +15,11 @@ const correl8 = function(doctype, basename, clientOpts) {
 
   let self = this;
   this.INDEX_BASE = basename || 'correl8-elastic';
-  this._type = doctype;
-  this._index = (self.INDEX_BASE + '-' + this._type).toLowerCase();
+  // this._type = doctype;
+  this._index = (self.INDEX_BASE + '-' + doctype).toLowerCase();
   this.configIndex = self._index + '-config';
-  this.configType = self.configIndex; // still required for 6.0 alpha
+  this.configId = self.configIndex;
+  // this.configType = self.configIndex; // still required for 6.0 alpha
 
   this.getClientOpts = () => {
     return JSON.parse(JSON.stringify(clientOpts));
@@ -31,17 +32,29 @@ const correl8 = function(doctype, basename, clientOpts) {
   // config index created automatically if it doesn't exist already
   // should be blocking
   client.indices.exists({index: self.configIndex}).then((res) => {
-    if (!res) {
-      return client.index({
+    if (!res || res.statusCode == 404) {
+      return client.indices.create({
         index: self.configIndex,
-        type: self.configType,
         body: {}
       }).then(() => {
         // console.log('Created config index');
       }).catch((error) => {
-        console.warn('Could not create config index! ' + error);
+        console.error('Could not create config index! ');
+        console.trace(new Error(error));
       });
     }
+  }).catch((error) => {
+    // console.error('Could not read config index! ');
+    // console.trace(error);
+    return client.indices.create({
+      index: self.configIndex,
+      body: {}
+    }).then(() => {
+      // console.log('Created config index');
+    }).catch((error) => {
+      console.error('Could not create config index! ');
+      console.trace(new Error(error));
+    });
   });
 
   this.index = (newName) => {
@@ -53,7 +66,7 @@ const correl8 = function(doctype, basename, clientOpts) {
 
   this.type = (newName) => {
     if (newName) {
-      self._type = newName;
+      // self._type = newName;
       self._index = self.INDEX_BASE + '-' + newName.toLowerCase();
     }
     return self;
@@ -62,10 +75,10 @@ const correl8 = function(doctype, basename, clientOpts) {
   this.config = (object) => {
     let params = {
       index: self.configIndex,
-      type: self.configType
+      // type: self.configType
     };
     let searchParams = Object.assign({}, params); // create copy
-    searchParams.q = '_id:' + self.configType;
+    searchParams.q = '_id:' + self.configId;
     searchParams.body = {size: 1};
     if (object) {
       return client.search(searchParams).then((response) => {
@@ -76,31 +89,46 @@ const correl8 = function(doctype, basename, clientOpts) {
           return client.update(params);
         }
         else {
-          params.id = self.configType;
+          params.id = self.configId;
           params.body = object;
           return client.index(params);
         }
+      }).catch((error) => {
+        console.error('Found invalida configuration! ');
+        console.trace(new Error(error));
       });
     }
     return client.search(searchParams);
   };
 
   this.init = (object) => {
-    var properties = createMapping(object)
-    properties.timestamp = {type: 'date', format: 'strict_date_optional_time||epoch_millis'};
+    var properties;
+    if (object.mappings && object.mappings.properties) {
+      // assume that passed object is a valid mapping specification
+      properties = object.mappings.properties;
+    }
+    else {
+      properties = createMapping(object);
+    }
+    // override in any case
+    properties["@timestamp"] = {type: 'date', format: 'strict_date_optional_time||epoch_millis'};
+    // console.log(JSON.stringify(properties, null, 1));
     return client.index({
       index: self._index,
-      type: self._type,
+      // type: self._type,
       body: {}
     }).then(() => {
       return client.indices.putMapping({
         index: self._index,
-        type: self._type,
-        include_type_name: true,
+        // type: self._type,
+        // include_type_name: true,
         body: {properties: properties}
       }).then(() => {
         // console.log(self._index + " initialized");
       });
+    }).catch((error) => {
+      console.error('Could not initialize ' + self._index);
+      console.trace(new Error(error));
     });;
   };
 
@@ -109,34 +137,50 @@ const correl8 = function(doctype, basename, clientOpts) {
   };
 
   this.clear = () => {
-    return client.indices.getMapping({index: self._index}).then((map) => {
+    return client.indices.getMapping({index: self._index}).then((resp) => {
       // console.log('Fetched index map');
+      // console.log(resp.body);
+      // console.log(self._index);
       return client.indices.delete({index: self._index}).then(() => {
         // console.log('Deleted index');
         return client.index({
           index: self._index,
-          type: self._type,
-          include_type_name: true,
+          // type: self._type,
+          // include_type_name: true,
           body: {}
         }).then(() => {
-          var mappings = map[self._index].mappings;
-          if (mappings && mappings[self._type] && mappings[self._type].properties) {
-            if (mappings[self._type].properties.fielddata) {
-              delete(mappings[self._type].properties.fielddata);
+          var mappings = resp.body[self._index].mappings;
+          if (mappings && mappings.properties) {
+            if (mappings.properties.fielddata) {
+              delete(mappings.properties.fielddata);
             }
+            // console.log(mappings.properties);
             return client.indices.putMapping({
               index: self._index,
-              type: self._type,
-              include_type_name: true,
-              body: {properties: mappings[self._type].properties}
+              // type: self._type,
+              // include_type_name: true,
+              body: {properties: mappings.properties}
             });
           }
           else {
             console.warn('No existing mapping, set mapping with init');
           }
+        }).catch((error) => {
+          console.error('Could not create mapping for ' + self._index);
+          console.trace(new Error(error));
         });
+      }).catch((error) => {
+        console.error('Could not delete existing index ' + self._index);
+        console.trace(new Error(error));
       });
+    }).catch((error) => {
+      console.error('Could not read mapping for existing index ' + self._index);
+      console.trace(new Error(error));
     });
+  };
+
+  this.getMapping = () => {
+    return client.indices.getMapping({index: self._index});
   };
 
   this.guessTime = (obj) => {
@@ -164,33 +208,39 @@ const correl8 = function(doctype, basename, clientOpts) {
   this.insert = (object) => {
     var ts = new Date();
     var guessed;
-    if (object && object.timestamp) {
-      if (guessed = self.guessTime(object.timestamp)) {
+    if (object && (object["@timestamp"] || object.timestamp)) {
+      let candidate = object["@timestamp"] || object.timestamp;
+      if (guessed = self.guessTime(candidate)) {
         ts = guessed;
       }
       else {
-        var msg = 'Could not parse timestamp ' + object.timestamp + '!' +
-          ' Overriding with ' + ts + '.';
+        var msg = 'Could not parse timestamp ' + candidate + '!' +
+          ' Setting @timestamp to ' + ts + '.';
         console.warn(msg);
       }
     }
-    object.timestamp = ts;
+    object["@timestamp"] = ts;
     return client.indices.exists({index: self._index}).then(() => {
       // console.log('Index exists!');
       var params = {
         index: self._index,
-        type: self._type,
+        // type: self._type,
         body: object
       };
       if (object.id) {
         params.id = object.id;
       }
+      console.log('INSERTING: ' + JSON.stringify(params, null, 1));
       return client.index(params);
+    }).catch((error) => {
+      console.error('Initialize first! Unknown index ' + self._index);
+      console.trace(error);
+      throw new Error(error);
     });
   };
 
   this.bulk = (bulk, timeout) => {
-    var params = {index: self._index, type: self._type, body: bulk};
+    var params = {index: self._index, body: bulk};
     if (timeout) {
       params.timeout = timeout;
       params.requestTimeout = 300000;
@@ -199,7 +249,7 @@ const correl8 = function(doctype, basename, clientOpts) {
   };
 
   this.deleteOne = (id) => {
-    return client.delete({index: self._index, type: self._type, id: id});
+    return client.delete({index: self._index, id: id});
   };
 
   this.deleteMany = (params) => {
@@ -208,42 +258,48 @@ const correl8 = function(doctype, basename, clientOpts) {
       var bulk = [];
       for (var i=0; i<results.lenght; i++) {
         var id = results[i]._id;
-        bulk.push({delete: {index: this._index, type: this._type, id: id}});
+        bulk.push({delete: {index: this._index, id: id}});
       }
       // console.log(bulk);
-      this.bulk(bulk);
+      return this.bulk(bulk);
+    }).catch((error) => {
+      console.warn('Could not search ' + self._index);
+      console.trace(new Error(error));
     });
   };
 
   this.remove = () => {
-    return client.indices.delete({index: this._index}).then((result) => {
-      return client.indices.delete({index: this.configIndex});
+    return client.indices.delete({index: self._index}).then((result) => {
+      return client.indices.delete({index: self.configIndex});
+    }).catch((error) => {
+      console.warn('Could not delete index ' + self._index);
+      console.trace(new Error(error));
     });
   };
 
   this.startScroll = (params) => {
     params.index = self._index;
-    params.type = self._type;
+    // params.type = self._type;
     return client.search(params);
   };
 
   this.scroll = (params) => {
-    params.index = self._index;
-    params.type = self._type;
+    // params.index = self._index;
+    // params.type = self._type;
     return client.scroll(params);
   };
 
   this.search = (params) => {
-    return client.search({index: self._index, type: self._type, body: params});
+    return client.search({index: self._index, body: params});
   };
 
   this.msearch = (params) => {
     var searchParams = [];
     for (var i=0; i<params.length; i++) {
-      searchParams[2*i] = {index: self._index, type: self._type};
+      searchParams[2*i] = {index: self._index};
       searchParams[2*i+1] = params[i];
     }
-    return client.msearch({index: self._index, type: self._type, body: searchParams});
+    return client.msearch({index: self._index, body: searchParams});
   };
 
   this.release = () => {
@@ -254,6 +310,12 @@ const correl8 = function(doctype, basename, clientOpts) {
   this.trimResults = (r) => {
     if (r && r.body && r.body.hits && r.body.hits.hits && r.body.hits.hits[0] && r.body.hits.hits[0]._source) {
       return r.body.hits.hits[0]._source;
+    }
+    else if (r && r.body) {
+      return r.body;
+    }
+    else if (r && r.meta && r.meta.body) {
+      return r.meta.body;
     }
     return false;
   };
